ef1524962be4a95589fe12e01eee5d1942600d2a
Sharon Modiz <sharonmodiz@yahoo.com>
2014-10-29 22:05:27 -0600

    Simplified it and used a class constant

--- ruby/gigasecond/gigasecond.rb
+++ ruby/gigasecond/gigasecond.rb
@@ -3,18 +3,11 @@ require 'time'
 
 class Gigasecond
 
+  @@gigasec = 10**9
+
   def self.from(date)
-    date_for(seconds_for(date) + 10**9)
+    (date.to_time + @@gigasec).to_date
   end
 
-  private
-
-  def self.seconds_for(date)
-    date.to_time.to_i
-  end
-
-  def self.date_for(seconds)
-    Time.at(seconds).to_date
-  end
 end
 
